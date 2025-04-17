3e6a638e90b9ccd2d86d6ede63dec5f99dfd4135
Juan Carlos <95167725+juan-C96@users.noreply.github.com>
2025-04-17 09:38:58 +0200

    test page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
-    pathMatch: 'full'
+    component: LayoutComponent, 
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    path: 'quienes-somos',
+    loadComponent: () =>
+      import('./components/layout/layout.component').then(m => m.LayoutComponent),
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    LayoutComponent,
   ],
   exports: [RouterModule]
 })
